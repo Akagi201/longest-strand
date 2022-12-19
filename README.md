838ac7f9a6f03e243445dbf20f1f83ea9f43eaa5
Akagi201 <akagi201@gmail.com>
2022-12-19 17:39:53 +0800

    feat: the lcs solution

--- README.md
+++ README.md
@@ -11,3 +11,19 @@ The program should display:
 - the length of the strand
 - the file names where the largest strand appears
 - the offset where the strand appears in each file
+
+## Build && Run
+
+```sh
+cargo run --release # the release profile is optimized
+```
+
+## Solution
+
+1. Open each file in binary mode and read the contents into memory.
+2. Iterate through each file, comparing the contents to each other file.
+3. For each comparison, find the longest common substring using a string comparison algorithm such as the Longest Common Subsequence (LCS) algorithm.
+4. If a longer common substring is found, update the length of the longest strand and the file names and offsets where it appears.
+5. Once all comparisons have been made, output the length of the longest strand, the file names where it appears, and the offsets where it appears in each file.
+
+This solution has a time complexity of O(n^2 * m) where n is the number of files and m is the length of the longest file, so it may not be practical for very large files or a large number of files.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,90 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // Get the list of files from the command line arguments
+    // Get the list of file names that match the pattern "sample.*" in the "samples" directory
+    let dir = "samples";
+    let files: Vec<String> = fs::read_dir(dir)
+        .unwrap()
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| entry.file_name().to_str().unwrap().starts_with("sample."))
+        .map(|entry| entry.path())
+        .map(|path| path.to_str().unwrap().to_string())
+        .collect();
+    println!("Dealing with samples: {files:?}");
+
+    // Read the contents of each file into memory
+    let mut contents = Vec::new();
+    for file in &files {
+        let file_contents = fs::read(file).expect("Error reading file");
+        contents.push(file_contents);
+    }
+
+    // Initialize variables to track the longest strand
+    let mut longest_strand = 0;
+    let mut file_names = Vec::new();
+    let mut offsets = Vec::new();
+
+    // Compare each file to every other file
+    for i in 0..files.len() {
+        for j in i + 1..files.len() {
+            // Find the longest common substring using the LCS algorithm
+            let lcs = lcs(&contents[i], &contents[j]);
+
+            // Update the longest strand if a longer one is found
+            if lcs.len() > longest_strand {
+                longest_strand = lcs.len();
+                file_names = vec![&files[i], &files[j]];
+                offsets = vec![
+                    contents[i].iter().position(|&b| b == lcs[0]).unwrap(),
+                    contents[j].iter().position(|&b| b == lcs[0]).unwrap(),
+                ];
+            }
+        }
+    }
+
+    // Output the results
+    println!("Longest strand: {longest_strand} bytes");
+    println!("File names: {file_names:?}");
+    println!("Offsets: {offsets:?}");
+}
+
+fn lcs(x: &[u8], y: &[u8]) -> Vec<u8> {
+    // Initialize the LCS table
+    let m = x.len();
+    let n = y.len();
+    let mut l = vec![vec![0; n + 1]; m + 1];
+
+    // Fill in the LCS table
+    for i in 0..=m {
+        for j in 0..=n {
+            if i == 0 || j == 0 {
+                l[i][j] = 0;
+            } else if x[i - 1] == y[j - 1] {
+                l[i][j] = l[i - 1][j - 1] + 1;
+            } else {
+                l[i][j] = std::cmp::max(l[i - 1][j], l[i][j - 1]);
+            }
+        }
+    }
+
+    // Backtrack to find the LCS
+    let mut i = m;
+    let mut j = n;
+    let mut index = l[i][j];
+    let mut lcs = Vec::with_capacity(index as usize);
+    while i > 0 && j > 0 {
+        if x[i - 1] == y[j - 1] {
+            lcs.push(x[i - 1]);
+            i -= 1;
+            j -= 1;
+            index -= 1;
+        } else if l[i - 1][j] > l[i][j - 1] {
+            i -= 1;
+        } else {
+            j -= 1;
+        }
+    }
+    lcs.reverse();
+    lcs
 }
